50760ebf8c833764b71243dffdfc0f445a891f22
Novazava <novaneybl@gmail.com>
2024-05-28 13:41:21 +0500

    Update votjs.js

--- votjs.js
+++ votjs.js
@@ -87,8 +87,8 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 let usercard = document.getElementById("usercard");
 let usercard = document.createElement("p");
 
-p.innerText = `${initDataUnsafe.first_name}
-${initDataUnsafe.last_name}`;
+p.innerText = `${initDataUnsafe.user.first_name}
+${initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
 
